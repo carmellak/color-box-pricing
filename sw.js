1b4f8e2fad5bcf9aa5b583d90a394bf7de4ae84a
carmellak <2172776376@qq.com>
2026-08-09 10:05:50 +0800

    feat: 网络优先+自动更新

--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
 const CACHE = 'caihe-v1';
-const FILES = [
-  '/color-box-pricing/',
-  '/color-box-pricing/index.html',
-  '/color-box-pricing/manifest.json',
-];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(
-    keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-  )));
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
+// 网络优先：有网就拿最新，没网才用缓存
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+    fetch(e.request).then(resp => {
       if (resp.ok) {
         const clone = resp.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
       }
       return resp;
-    }))
+    }).catch(() => caches.match(e.request))
   );
+});
+
+// 新版本检测：发现新 SW 立即激活
+self.addEventListener('message', e => {
+  if (e.data === 'skipWaiting') self.skipWaiting();
 });
